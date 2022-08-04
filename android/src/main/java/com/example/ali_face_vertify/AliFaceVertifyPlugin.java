104ab58aea296fbb9eeab6f6cc0e16b613585bef
李畅 <1027959757@outlook.com>
2022-08-04 11:30:11 +0800

    fix：添加安卓原因映射

--- android/src/main/java/com/example/ali_face_vertify/AliFaceVertifyPlugin.java
+++ android/src/main/java/com/example/ali_face_vertify/AliFaceVertifyPlugin.java
@@ -1,6 +1,7 @@
 package com.example.ali_face_vertify;
 
 import android.content.Context;
+import android.util.Log;
 import androidx.annotation.NonNull;
 import com.alibaba.fastjson.JSON;
 import com.aliyun.aliyunface.api.ZIMCallback;
@@ -15,9 +16,9 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
-
 import java.util.HashMap;
 import java.util.Map;
+
 
 
 /** AliFaceVertifyPlugin */
@@ -29,7 +30,8 @@ public class AliFaceVertifyPlugin implements FlutterPlugin, MethodCallHandler, A
   static AliFaceVertifyManager.AliFaceVertifyCallBack aliFaceVertifyCallBack;
   private MethodChannel channel;
   private Context context;
-  
+  public static String jsonReasonMapString = "{\"Z5120\":\"刷脸成功，认证通过\",\"Z1000\":\"其他异常 \",\"Z1001\":\"人脸识别算法初始化失败 \",\"Z1003\":\"不支持的CPU架构 \",\"Z1004\":\"Android系统版本过低 \",\"Z1005\":\"刷脸超时(单次) \",\"Z1006\":\"多次刷脸超时 \",\"Z1018\":\"无前置摄像头 \",\"Z1019\":\"摄像头权限未赋予 \",\"Z1020\":\"打开摄像头失败 \",\"Z1024\":\"SDK认证流程正在进行中，请等待 \",\"Z1029\":\"Android系统不支持录屏 \",\"Z1030\":\"录音权限未赋予 \",\"Z1031\":\"录屏权限未赋予 \",\"Z1032\":\"麦克风打开失败 \",\"Z1033\":\"录屏打开失败 \",\"Z1034\":\"本地存储空间过小\",\"Z5116\":\"音频文件上传失败 \",\"Z5112\":\"上传炫彩Meta信息失败 \",\"Z5113\":\"上传炫彩视频失败 \",\"Z5114\":\"认证视频上传失败 \",\"Z6001\":\"OCR识别次数超限 \",\"Z6002\":\"OCR图片上传网络超时 \",\"Z6003\":\"OSS Token过期 \",\"Z6004\":\"人脸照片处理失败 \",\"Z7001\":\"SDK初始化或者使用过程中数据异常 \",\"Z1008\":\"用户在认证过程中退出 \",\"Z1009\":\"用户在授权页面点击“暂不授权”退出 \",\"Z1011\":\"客户端初始化网络错误 \",\"Z1012\":\"客户端网络访问异常 \",\"Z1025\":\"客户端初始化接口返回网络错误 \",\"Z1026\":\"信息上传网络错误 \",\"Z1027\":\"服务端认证接口网络错误 \",\"Z5128\":\"刷脸失败，认证未通过 可通过服务端查询接口获取认证未通过具体原因 \"}";
+  public Map<String,Object> reasonCodeMap;
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
     onAttachedToEngine(binding.getApplicationContext(), binding.getBinaryMessenger());
@@ -41,6 +43,7 @@ public class AliFaceVertifyPlugin implements FlutterPlugin, MethodCallHandler, A
     channel.setMethodCallHandler(this);
     AliFaceVertifyManager.AliFaceVertifyManagerImpl.setup(flutterPluginBinding,this);
     aliFaceVertifyCallBack = new AliFaceVertifyManager.AliFaceVertifyCallBack(flutterPluginBinding);
+    reasonCodeMap = JSON.parseObject(jsonReasonMapString);
 }
 
   @Override
@@ -83,12 +86,16 @@ public class AliFaceVertifyPlugin implements FlutterPlugin, MethodCallHandler, A
       ZIMFacade zimFacade = ZIMFacadeBuilder.create(this.context);
 
       HashMap<String, String> extParams = new HashMap<>();
-
+      extParams.put(ZIMFacade.ZIM_EXT_PARAMS_KEY_OCR_BOTTOM_BUTTON_COLOR, "##4F65EE");
       zimFacade.verify(certifyId, true, extParams, new ZIMCallback() {
           public boolean response(final ZIMResponse response) {
+            /// 阿里iOS中有直接的reason,安卓中response.reason只有code需要对doc建立映射
+            Object localDocReason = reasonCodeMap.get(response.reason);
+            String reason = String.format("%s",localDocReason);
+            Log.d("AliFaceVerify","ZIMResponse return: \ncode:"+response.code+"\nreason:"+response.reason+"  "+reason+"\nmsg:"+response.msg);
               AliFaceVertifyManager.FaceVerResponse faceRsp = new AliFaceVertifyManager.FaceVerResponse();
               faceRsp.setCode((long) response.code);
-              faceRsp.setReason(response.reason);
+              faceRsp.setReason(reason);
               faceRsp.setRetMessageSub(response.msg);
               faceRsp.setDeviceToken(response.deviceToken);
               faceRsp.setVideoFilePath(response.videoFilePath);
